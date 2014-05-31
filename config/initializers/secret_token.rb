7312c4ff67c00968dc6ad5df20fdfbbcd7132808
Jeffrey Tong <trigun0x2@gmail.com>
2014-05-31 15:04:54 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blinderino::Application.config.secret_token = '860a029685acc556896400d1047f4b9c0bd8404cbe73c0fca7e0d8c84fee8c07289ff7a1b7ae61af44586a35252aedd2df1dec3a788d0c0b3953afde679e1727'
